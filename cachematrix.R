849f9a57cba42893d837032cc25042cd7ca22620
dhja <dhirenjani@users.noreply.github.com>
2014-04-22 11:30:04 +0530

    Submitted Code
    As per assignment needs

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions will take a matrix and provide it's inverse
+## We assume that the input data is always correct and matrix inversion is possible
 
-## Write a short comment describing this function
+## This function will cache the inverse of a matrix in memory
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+  m <<- NULL
+  p <- x
+  m <- solve(p)
+## a sample matrix is matrix(data=c(1:4,2:5,c(4,7,3,1),c(2,3,1,5)),nrow=4,ncol=4)
 }
 
 
-## Write a short comment describing this function
+##This function will return the reverse of a matrix. 
+##First it will check if the reverse already exists.
+##Then it will return the reverse from cache.
+##Else it will calculate the reverse using 
+##the solve() function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+  ## Return a matrix that is the inverse of 'x'
+  if(!is.null(m))
+  {
+    message("getting cached data")
+    return(m)
+  }
+  r <- solve(x)
+  return(r)
 }
